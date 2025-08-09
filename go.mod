b66a2eff0fb91643788fb662058cabba0a760814
villa <lixuguang@papegames.net>
2025-08-09 23:01:17 +0800

    feat: update code

---
+++ go.mod
@@ -0,0 +1,3 @@
+module vscode
+
+go 1.23.7
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello world2")
+
+	fmt.Println("hello w")
+	fmt.Println("hell world4")
+	fmt.Println("hell world4")
+	fmt.Println("hello world5")
+}
